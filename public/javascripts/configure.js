957c96c1c119bc1afd940b35d2e0a86f3ec34436
Akshay Deshraj <akshay30nov@gmail.com>
2016-10-22 19:29:42 +0530

    Subscribe channel event added

---
+++ public/javascripts/configure.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    $(".btn").click(function (event) {
+        event.preventDefault();
+
+        console.log('test');
+
+        // Find out group selected
+        var selectedGroup = $("#sel1").find(":selected");
+        var groupName = selectedGroup.text();
+        var groupId = selectedGroup.attr('id');
+
+        var incomingUrl = $("#inputIWebhook").val();
+
+        var jsonObjectToPost = {
+            groupId: groupId,
+            f_channel: "hackathon",
+            group_name: groupName,
+            webhook_url: incomingUrl,
+            associated_user: userId
+        };
+
+        console.log('jsonObject', jsonObjectToPost);
+
+        $.post("https://d64f278b.ngrok.io/flocknet/subscribe-channel",
+            jsonObjectToPost)
+            .done(function (msg) {
+                console.log('Message', msg);
+            })
+            .fail(function (xhr, status, error) {
+                console.log('JSON Object', jsonObjectToPost);
+                console.log('XHR', xhr);
+                console.log('status', status);
+                console.log('error', error);
+            })
+
+    });
+});
