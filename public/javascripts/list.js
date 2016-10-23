aab508a03e87f2f858fe2f5cf23e0b5f14508a07
aptro <akashkupatro@gmail.com>
2016-10-23 08:56:14 +0530

    action menu refactor and clicks removed

--- public/javascripts/list.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-
-    $("#channels").find("li").click(function () {
-        console.log($(this).attr('id'));
-        var channel = $(this).attr('id');
-        window.open('https://d64f278b.ngrok.io/flocknet/configure-action?channel=' + channel + '&' + 'flockEventToken=' + v_token);
-
-    });
-});
